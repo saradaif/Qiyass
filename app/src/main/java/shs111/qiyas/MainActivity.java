61c2d79a66611048d40a34b8d560c50301825d64
saradaif <s.d.a.s@hotmail.com>
2016-03-27 21:53:19 +0300

    update

--- app/src/main/java/shs111/qiyas/MainActivity.java
+++ app/src/main/java/shs111/qiyas/MainActivity.java
@@ -1,60 +1,98 @@
 package shs111.qiyas;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public static final String USER_NAME = "USER_NAME";
+
+    public static final String PASSWORD = "PASSWORD";
+
+    private static final String LOGIN_URL = "http://127.0.0.1/group chatting for qiyas mobile application/login.php";
+
+    private EditText editTextUserName;
+    private EditText editTextPassword;
+
+    private Button buttonLogin;
+    private Button buttonRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button login=(Button)findViewById(R.id.loginButton);
-        Button newlogin=(Button)findViewById(R.id.newloginButton2);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i=new Intent(MainActivity.this,ChatSetting.class);
-                startActivity(i);
-            }
-        });
-        newlogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i=new Intent(MainActivity.this,Registration.class);
-                startActivity(i);
-            }
-        });
 
+        editTextUserName = (EditText) findViewById(R.id.nameEditText);
+        editTextPassword = (EditText) findViewById(R.id.passwordEditText2);
+
+        buttonLogin = (Button) findViewById(R.id.loginButton);
+        buttonRegister = (Button) findViewById(R.id.newloginButton);
+
+        buttonLogin.setOnClickListener(this);
+        buttonRegister.setOnClickListener(this);
     }
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View v) {
+        if(v == buttonLogin){
+            startActivity(new Intent(this, ChatSetting.class));
+            login();
         }
+        if(v == buttonRegister){
+            startActivity(new Intent(this, Registration.class));
+        }
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void login(){
+        String username = editTextUserName.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        userLogin(username, password);
+    }
+
+    private void userLogin(final String username, final String password){
+        class UserLoginClass extends AsyncTask<String,Void,String> {
+            ProgressDialog loading;
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(MainActivity.this,"Please Wait",null,true,true);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                if(s.equalsIgnoreCase("success")){
+                    Intent intent = new Intent(MainActivity.this,ChatSetting.class);
+                    intent.putExtra(USER_NAME,username);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(MainActivity.this, s, Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+                HashMap<String,String> data = new HashMap<>();
+                data.put("username",params[0]);
+                data.put("password",params[1]);
+
+                RegisterUserClass ruc = new RegisterUserClass();
+
+                String result = ruc.sendPostRequest(LOGIN_URL,data);
+
+                return result;
+            }
+        }
+        UserLoginClass ulc = new UserLoginClass();
+        ulc.execute(username, password);
     }
 }
--- app/src/main/java/shs111/qiyas/Registration.java
+++ app/src/main/java/shs111/qiyas/Registration.java
@@ -1,36 +1,101 @@
 package shs111.qiyas;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.util.HashMap;
 
-public class Registration extends AppCompatActivity {
+public class Registration extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private EditText editTextConfPassword;
+    private EditText editTextEmail;
+    private EditText editTextConfEmail;
+
+    private Button buttonRegister;
+
+    private static final String REGISTER_URL = "http://group chatting for qiyas mobile application/register.php";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
-        Button ok=(Button)findViewById(R.id.okButton);
-        Button back=(Button)findViewById(R.id.backButton);
-        ok.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i=new Intent(Registration.this,GroupsSetting.class);
-                startActivity(i);
-            }
-        });
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-             Intent i=new Intent(Registration.this,MainActivity.class);
-                startActivity(i);
-            }
-        });
+        setContentView(R.layout.activity_main);
+
+        editTextUsername = (EditText) findViewById(R.id.nameEditText);
+        editTextPassword = (EditText) findViewById(R.id.passwordEditText2);
+        editTextConfPassword = (EditText) findViewById(R.id.confpasswordEditText1);
+        editTextEmail = (EditText) findViewById(R.id.EmaileditText4);
+        editTextConfEmail = (EditText) findViewById(R.id.confEmailEditText5);
+
+        buttonRegister = (Button) findViewById(R.id.okButton);
+
+        buttonRegister.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        if(v == buttonRegister) {
+            registerUser();
+        }
+        if(v == buttonRegister) {
+            startActivity(new Intent(this, GroupsSetting.class));
+        }
+    }
+
+    private void registerUser() {
+        String username = editTextUsername.getText().toString().trim().toLowerCase();
+        String password = editTextPassword.getText().toString().trim().toLowerCase();
+        String confpassword = editTextConfPassword.getText().toString().trim().toLowerCase();
+        String email = editTextEmail.getText().toString().trim().toLowerCase();
+        String confemail = editTextConfEmail.getText().toString().trim().toLowerCase();
+
+        register(username,password,confpassword ,email,confemail);
+    }
+
+    private void register(String username, String password, String confpassword, String email, String confemail) {
+        class RegisterUser extends AsyncTask<String, Void, String>{
+            ProgressDialog loading;
+            RegisterUserClass ruc = new RegisterUserClass();
+
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(Registration.this, "Please Wait",null, true, true);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                HashMap<String, String> data = new HashMap<String,String>();
+                data.put("username",params[0]);
+                data.put("password",params[1]);
+                data.put("confpassword",params[2]);
+                data.put("email",params[3]);
+                data.put("confemail",params[3]);
+
+                String result = ruc.sendPostRequest(REGISTER_URL,data);
+
+                return  result;
+            }
+        }
+
+        RegisterUser ru = new RegisterUser();
+        ru.execute(username,password,confpassword,email, confemail);
+    }
 }
